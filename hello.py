ed2f0b3618132d2a68fcfa5bf232d6d3d30d4fe4
Kene Arionye <kenechiaugustine@gmail.com>
2022-07-24 22:35:46 +0100

    comments and inputs

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 my_name = "Kene"
-print('Hello', my_name)
+# Receive a name from the user
+your_name = input("What is your name? ")
+# Print a greeting
+print("Hello, " + your_name + "!")
+# print('Hello', my_name)
