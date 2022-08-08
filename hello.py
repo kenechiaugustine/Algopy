81a7adc460a1bd98a0f008d1bd575681ba142275
Kene Arionye <kenechiaugustine@gmail.com>
2022-08-08 21:43:06 +0100

    rewrote everything i learnt

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
+# Printing in console...
 print('Hello boss!!!')
+
+# Declaring variables
+my_name = "Kene"
+print('Hello,' + my_name)
+
+# Receive a name from the user
+your_name = input("What is your name? ")
+print("Hello, " + your_name + "!")
+
+# Functions
+def my_function():
+  print("Hello from a function")
+my_function()
+
