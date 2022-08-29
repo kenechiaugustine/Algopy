28be809dd1036312981a327eff9e4a53056f1da6
Kene Arionye <kenechiaugustine@gmail.com>
2022-08-29 09:22:42 +0100

    added counter

--- permutation.py
+++ permutation.py
@@ -1,9 +1,14 @@
-from itertools import permutations
-def allpermutations(str):
-    permList = permutations(str)
+from itertools import count, permutations
+
+
+def allpermutations(string_input):
+    total_permutation = 0
+    permList = permutations(string_input)
     for perm in permList:
+        total_permutation = total_permutation + 1
         print(''.join(perm))
+    print('There were ' + str(total_permutation) + ' possible permutation')
 
 if __name__ == '__main__':
-    str = input('Enter a string: ')
-    allpermutations(str)
+    string_input = input('Enter a string: ')
+    allpermutations(string_input)
